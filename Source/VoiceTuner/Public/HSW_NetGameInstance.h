cf8cd30413ba11167077d0a721a75c0f34230f1b
한승우 <tmddn_00@naver.com>
2024-09-12 16:31:31 +0900

    [Add] Robby맵, GameInatance, 배치파일

---
+++ Source/VoiceTuner/Public/HSW_NetGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "HSW_NetGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOICETUNER_API UHSW_NetGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ VoiceTuner_HSW.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Epic Games\UE_5.4\Engine\Binaries\Win64\UnrealEditor.exe" "C:\Projects\Unreal\VoiceTuner\VoiceTuner.uproject" "HSW_Stage" -game -ResX=640 -ResY=480 -WinX=200 -WinY=300 -log
